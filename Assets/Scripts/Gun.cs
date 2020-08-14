6e3276d37f50dece1fd939ffb5864b51ddbf1c78
Ethan <ethanholen@gmail.com>
2020-08-13 18:36:35 -0600

    gun works but the bullets sometimes wont impact the target, may be a problem with unity though and not fixable

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,16 +7,15 @@ public class Gun : MonoBehaviour
     public float speed = 40;
     public GameObject bullet;
     public Transform barrel;
-    public AudioSource audioSource;
-    public AudioClip audioClip;
+    // public AudioSource audioSource;
+    // public AudioClip audioClip;
 
     public void Fire()
     {
         GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
-        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
-        audioSource.PlayOneShot(audioClip);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = barrel.forward * speed;
+        // audioSource.PlayOneShot(audioClip);
         Destroy(spawnedBullet, 2);
-        
     }
 
 }
